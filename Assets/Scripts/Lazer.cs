48cb018293bd8911727448377721e4c0dca4898a
mariacharm <charm.granada@yahoo.com>
2013-10-06 22:24:49 +0800

    add all files

---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lazer : MonoBehaviour {
+
+	public Vector3 LazerPos = new Vector3(1,1,-3);
+	
+	// Use this for initialization
+	void Start () {
+		//LazerPos = new Vector3(1,1,-3);
+		transform.localPosition = LazerPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Debug.Log(Time.deltaTime);
+		
+		transform.Translate(Vector3.back * 0.1f, Space.World);
+		//transform.Translate(Vector3.back * 0.1f);
+		
+		if(transform.localPosition.z < -9)
+			DestroyObject(this.gameObject);
+			
+	}
+}
---
+++ Assets/Scripts/iTween_MoveTo.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iTween_MoveTo : MonoBehaviour {
+	
+	public GameObject cube;
+	private int x,y,z;
+	private Vector3 initialPos;
+	
+	public GameObject Lazer;
+	
+	private Vector3 newPos;
+	
+	// Use this for initialization
+	void Start () {
+		cube = GameObject.Find("Cube_green");
+		initialPos = transform.position;
+		newPos = initialPos;
+		x = (int)initialPos.x;
+		y = (int)initialPos.y;
+		z = (int)initialPos.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		CubePosition();
+		ShootAction ();
+	}
+	
+	// Cube Left - Right
+	void CubePosition () {	
+		
+		if(Input.GetKey(KeyCode.RightArrow)){
+			x++;
+		}		
+		else if(Input.GetKey(KeyCode.LeftArrow)){
+			x--;
+		}
+		
+		newPos = new Vector3(x, y, z);
+		cube.transform.position = newPos;
+	}
+	
+	// Cube Shoots
+	void ShootAction () {
+		
+		//GameObject Lazers = GameObject.Find("Cube_lazer");
+		
+		if(Input.GetKeyDown(KeyCode.A)){
+			Vector3 newPosA = new Vector3(1,1,-3);
+			
+			if(Lazer != null){
+				Debug.Log("HI" + Lazer);			
+			}
+			else{
+				Debug.Log("NO LAZER");
+				Instantiate(Lazer, newPosA, Quaternion.identity);
+			}
+		}
+	}
+}
